2d9eafcdf47e5915bbcc7d2f628c5a6e5f717437
Matěj Ryček <matejrycek@gmail.com>
2017-10-02 13:23:16 +0200

    Initial Commit

---
+++ todo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  todo
+//
+//  Created by Matěj Ryček on 02.10.17.
+//  Copyright © 2017 Matěj Ryček. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
