ba569e5c2a2c1cff661c01dfecc8d974fdfb235e
Matěj Ryček <matejrycek@gmail.com>
2017-10-02 13:50:14 +0200

    dalsi test

--- todo/main.cpp
+++ todo/main.cpp
@@ -8,7 +8,28 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
+    printf("hello, it work");
+    
+    int a = 3;
+    int b = 4;
+    int c = 6;
+    
+    
+    for (a; a > 5 ; a++) {
+        
+       cout << "value of a:" << a;
+        
+    }
+    if (c > 5)
+    {
+        printf("je to vetsi");
+    }
+    else
+        printf("je to mensi");
+    
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
